a557dc0aa09e9b87fc7f4c24663a26a726e1aba4
Rayen Labidi <89748200+IamRayen@users.noreply.github.com>
2022-03-01 12:47:14 +0100

    fixing deployment...

--- fend/src/Components/NavBar.js
+++ fend/src/Components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import logo from "../images/pdfresizer.com-pdf-crop.svg";
 import { Link, useNavigate } from "react-router-dom";
 
