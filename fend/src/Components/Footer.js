40903cf9060a60074f0dd38a824163d9dc00fae9
Rayen Labidi <89748200+IamRayen@users.noreply.github.com>
2022-03-09 23:31:38 +0100

    fixed footer css

--- fend/src/Components/Footer.js
+++ fend/src/Components/Footer.js
@@ -3,15 +3,17 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="footer-basic container-fluid">
-            <footer className="footer container-xl">
-                <div className="social container">
+            <footer
+                className="footer d-flex justify-content-around align-items-center flex-wrap flex-sm-nowrap container-xl"
+            >
+                <div className="social ">
                     <i className="btn btn-lg bi bi-linkedin"></i>
                     <i className="btn btn-lg bi bi-skype"></i>
                     <i className=" btn btn-lg bi bi-facebook"></i>
                     <i className="btn btn-lg bi bi-twitter"></i>
                     <i className="btn btn-lg bi bi-instagram"></i>
                 </div>
-                <ul className="list-inline container">
+                <ul className="list-inline ">
                     <li className="btn list-inline-item">
                         <a href="#landing" className="link">
                             Acceuil
@@ -43,25 +45,25 @@ const Footer = () => {
                         </a>
                     </li>
                 </ul>
-                <div className="copyright-wrapper container">
-                <p className="copyright">Oussama Habibi © 2022</p>
-                <p className="copyright">
-                    Website made by{" "}
-                    <a
-                        className="link00"
-                        style={{
-                            textDecoration: "none",
-                            fontWeight: "bold",
-                            color: "black",
-                        }}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        href="https://www.linkedin.com/in/rayenlabidi-full-stack-javascript-developer"
-                    >
-                        Rayen Labidi{" "}
-                        <i className="bi bi-box-arrow-up-right"></i>
-                    </a>
-                </p>
+                <div className="copyright-wrapper ">
+                    <p className="copyright">Oussama Habibi © 2022</p>
+                    <p className="copyright">
+                        Website made by{" "}
+                        <a
+                            className="link00"
+                            style={{
+                                textDecoration: "none",
+                                fontWeight: "bold",
+                                color: "black",
+                            }}
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            href="https://www.linkedin.com/in/rayenlabidi-full-stack-javascript-developer"
+                        >
+                            Rayen Labidi{" "}
+                            <i className="bi bi-box-arrow-up-right"></i>
+                        </a>
+                    </p>
                 </div>
             </footer>
         </div>
